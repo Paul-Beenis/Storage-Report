a9455a0e4e649e05fa26e7a1e17d545f555c8c76
Paul-Beenis <39440817+Paul-Beenis@users.noreply.github.com>
2018-10-01 08:54:48 -0600

    Update Storage-Report.ps1
    Adjusted and improved HTML

--- Storage-Report.ps1
+++ Storage-Report.ps1
@@ -25,91 +25,102 @@ $EmailBodySafeUsed=@()
 $EmailBodyFailed=@()
 $EmailBodyFailedName=@()
 $EmailBodyFailedLetter=@()
+$TD=@"
+<td style="border: 3px solid  #333333; padding: 4px;">
+"@
 
 #Iterate through each drive on each server
 foreach($Server in $ServerList){
     $drive = "$DriveLetter\"
     foreach($DriveLetter in $DriveLetters){
         try{
-            
-            # Gather Drive Variables
             $Drive = Get-WmiObject Win32_LogicalDisk -ComputerName $Server -Filter "DeviceID='$DriveLetter'" -ErrorAction Stop | Select-Object Size,FreeSpace
             $DriveSize = [Math]::Round($Drive.size / 1GB)
             $DriveFreeSpace = [Math]::Round($Drive.freespace / 1GB)
-            
             # Remove unused Drives
             if($DriveSize -eq 0){
+            
                 continue
                 }
-            
-            # Determine all Critical drives that are under safe threshold.
-            elseif($DriveFreeSpace -lt $SafeFreeSpaceGB){
-                $EmailBodyCritical+="
+
+            # Determine all other Safe drives, which is more than 10GB Free. If it is less than 10gb, mark for safe
+            elseif($DriveFreeSpace -lt 5){
+                $EmailBodyCriticalName="$TD$Server</td>"
+                $EmailBodyCriticalLetter="$TD$DriveLetter\</td>"
+                $EmailBodyCriticalUsed=("$TD$DriveFreeSpace/$DriveSize GB</td>")
+                
+                $EmailBodyCritical=$EmailBodyCritical+"
                 <tr>
-                <td>$Server</td>
-                <td>$DriveLetter\</td>
-                <td>$DriveFreeSpace/$DriveSize GB</td>
+                $EmailBodyCriticalName
+                $EmailBodyCriticalLetter
+                $EmailBodyCriticalUsed
                 </tr>
+
                 "
                 }
-           
-            # Mark as safe if not critical
             else{
-                $EmailBodySafe+="
+                $EmailBodySafeName="$TD$Server</td>"
+                $EmailBodySafeLetter="$TD$DriveLetter\</td>"
+                $EmailBodySafeUsed="$TD$DriveFreeSpace/$DriveSize GB</td>"
+                
+                $EmailBodySafe=$EmailBodySafe+"
                 <tr>
-                <td>$Server</td>
-                <td>$DriveLetter\</td>
-                <td>$DriveFreeSpace/$DriveSize GB</td>
+                $EmailBodySafeName
+                $EmailBodySafeLetter
+                $EmailBodySafeUsed
                 </tr>
                 "
                 }
             } 
-       
-       # Catch RPC / failed connection Errors 
         catch{ 
-            $EmailBodyFailed+="
-            <tr>
-            <td>$Server</td>
-            <td>$DriveLetter\</td>
-            </tr>
-            "
+                $EmailBodyFailedName="$TD$Server</td>"
+                $EmailBodyFailedLetter="$TD$DriveLetter</td>"
+                
+                $EmailBodyFailed=$EmailBodyFailed+"
+                <tr>
+                $EmailBodyFailedName
+                $EmailBodyFailedLetter
+                </tr>
+                "
         }
     }
 }
 
 
-# Email HTML format
 $HTML=@"
-<h2 style="font-family: verdana">Daily Server storage report for $Date</h1>
+<h2 style="font-family: verdana">Daily Server storage report for $Date</h2>
 &nbsp
 &nbsp
 <h3><b style="font-family: verdana;">Servers with critical storage shortage</b></h3>
-<table style="font-family: verdana;border: 3px solid #333333;">
-    <tr style="background: #eeeeee;border: 3px solid #333333;">
-        <th style="padding: 10px;border: 3px solid #333333;"><b>Server Name</b></th>
+<table style="border-collapse: collapse; border: 3px solid #333333; font-family: verdana; border-color: #333;">
+    <tr style="background: #eeeeee;border: 3px solid #333333;"><b>
+        <th style="padding: 10px;border: 3px solid #333333;">Server Name</th>
         <th style="padding: 10px;border: 3px solid #333333;">Drive Letter</th>
-        <th style="padding: 10px;border: 3px solid #333333;">Storage Used</th>
+        <th style="padding: 10px;border: 3px solid #333333;">Storage Left</th>
+        </b>
     </tr>
     $EmailBodyCritical
 </table>
 &nbsp
 &nbsp
 <h3><b style="font-family: verdana;">Servers with safe storage amounts</b></h3>
-<table style="border: 3px solid #333333;font-family: verdana;">
-    <tr style="background: #eeeeee;border: 3px solid #333333;">
-        <th style="padding: 10px;border: 3px solid #333333;"><b>Server Name</b></th>
+<table style="border-collapse: collapse; border: 3px solid #333333; font-family: verdana; border-color: #333;">
+    <tr style="background: #eeeeee;border: 3px solid #333333;"><b>
+        <th style="padding: 10px;border: 3px solid #333333;">Server Name</th>
         <th style="padding: 10px;border: 3px solid #333333;">Drive Letter</th>
-        <th style="padding: 10px;border: 3px solid #333333;">Storage Used</th>
+        <th style="padding: 10px;border: 3px solid #333333;">Storage Left</th>
+        </b>
     </tr>
     $EmailBodySafe
 </table>
 &nbsp
 &nbsp
-<h3><b style="font-family: verdana;">Servers that failed to provide storage</b></h3>
-<table style="font-family: verdana;border: 3px solid #333333;">
-    <tr style="background: #eeeeee;border: 3px solid #333333;">
-        <th style="padding: 10px;border: 3px solid #333333;"><b>Server Name</b></th>
+<h3><b style="font-family: verdana;">Servers with failed RPC connections</b></h3>
+<table style="border-collapse: collapse; border: 3px solid #333333; font-family: verdana; border-color: #333;">
+    <tr style="background: #eeeeee;border: 3px solid #333333;"><b>
+        <th style="padding: 10px;border: 3px solid #333333;">Server Name</th>
         <th style="padding: 10px;border: 3px solid #333333;">Drive Letter</th>
+        </b>
     </tr>
     $EmailBodyFailed
 </table>
